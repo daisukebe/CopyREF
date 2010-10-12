01f5f8841988a5d5bf93a55dd5221b3066fcd847
daisukebe <poleon.kd@gmail.com>
2010-10-12 08:50:36 -0400

    modify the example

--- content_script.js
+++ content_script.js
@@ -4,7 +4,8 @@
 	chrome.extension.sendRequest({greeting: text});
     }
 
-    //analyze DOM and get required information
+    // analyze DOM and get required information
+    // For example, the following code returns the title
     $(document.title);
 	
 })();
